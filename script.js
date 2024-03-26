fb38486a6685d5779a017c5a65225f6d73ac5504
Ishu <147903100+ishu8859@users.noreply.github.com>
2024-03-26 13:31:28 +0530

    create

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "Web Developer", "Java Developer", "YouTuber"],
+    typeSpeed : 50,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
